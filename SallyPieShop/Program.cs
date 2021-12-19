8be6c5f1faa0bf71b053e93eab8eb446de01486c
Lethukuthula Nsibande <lethunsibande16@gmail.com>
2021-12-19 11:46:41 +0200

    Update Program.cs
    Added comments on defaults

--- SallyPieShop/Program.cs
+++ SallyPieShop/Program.cs
@@ -11,16 +11,16 @@ namespace SallyPieShop
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) //A host is set up that will configure a server and a request processing pipeline
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); //CreateHostBuilder method will set up the application with some defaults
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); //UseStartup method specifies the startup class, which will perform the configuration of the application
                 });
     }
 }
