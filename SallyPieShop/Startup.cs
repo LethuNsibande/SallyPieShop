fcb25c068d23abc63a3042ebd077a45906d8c4c3
Lethukuthula Nsibande <lethunsibande16@gmail.com>
2021-12-19 11:58:38 +0200

    Update Startup.cs
    Added additional comments to defaults.

--- SallyPieShop/Startup.cs
+++ SallyPieShop/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace SallyPieShop
 {
-    public class Startup
+    public class Startup //This class configres all services needed throughout the application and it also defines the request pipeline
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -21,6 +21,12 @@ namespace SallyPieShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Middleware components are added here (Configure method)
+            //...middleware components are a number of components chained behind one another in the request pipeline,
+            //...they will intercept and or handle incoming HTTP request.
+            //...Each component will be able to alter the request or by the way, also the response can be altered, 
+            //...or simply pass it on to the next component in the request pipeline
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
